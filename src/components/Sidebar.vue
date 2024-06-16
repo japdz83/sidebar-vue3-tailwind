2150488ba7d3845413b8dbad319f0201dde7eb93
Johan Prado <japdz83@users.noreply.github.com>
2024-06-16 15:41:28 -0400

    Update Sidebar.vue
    Se agregan cambios en el funcionamiento

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,14 +6,14 @@ import { RouterLink } from 'vue-router'
 
 // }
 defineProps({
-        openSidebar: {
+        is_expanded: {
             type: Boolean
         }
     })
 </script>
 
 <template>
-	<aside class="h-screen w-[300px] bg-gray-800">
+	<aside class="h-screen w-[300px] bg-gray-800 " id="side-bar" :class="is_expanded === true ? 'side-bar-visible' : 'side-bar-close' ">
 		<div class="bg-gray-700 h-[50px]">
 			<div
 				class="text-xl font-bold text-center flex items-center text-white justify-center h-full"
@@ -24,12 +24,14 @@ defineProps({
 		<div
 			class="flex flex-col justify-between h-[calc(100vh-3rem)] bg-gray-900 py-5"
 		>
-			{{ openSidebar }}
 			<div class="menu-man text-left px-2 whitespace-nowrap">
 				<div
 					class="p-3 rounded-sm cursor-pointer text-gray-300 hover:text-white"
 				>
-					<router-link class="px-2 flex space-x-2 items-center">
+					<router-link
+						:to="{name: 'dashboard'}"
+						class="px-2 flex space-x-2 items-center"
+					>
 						<span class="pi pi-microsoft"></span>
 						<span class="">Dashboard</span>
 					</router-link>
@@ -38,7 +40,10 @@ defineProps({
 				<div
 					class="p-3 rounded-sm cursor-pointer text-gray-300 hover:text-white"
 				>
-					<router-link class="px-2 flex space-x-2 items-center">
+					<router-link
+						:to="{name: 'producto'}"
+						class="px-2 flex space-x-2 items-center"
+					>
 						<span class="pi pi-microsoft"></span>
 						<span class="">Producto</span>
 					</router-link>
@@ -48,4 +53,17 @@ defineProps({
 	</aside>
 </template>
 
-<style scoped></style>
+<style>
+#side-bar{
+	overflow: hidden;
+	transition: 300ms;
+}
+
+.side-bar-visible{
+	width: 300px !important;
+}
+
+.side-bar-close{
+	width: 50px !important;
+}
+</style>
